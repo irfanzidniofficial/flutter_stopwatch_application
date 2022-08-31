ef4f883bf60d293a006974d4ef34979fdbe28fa3
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-08-31 21:54:27 +0700

    logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,6 +27,79 @@ class HomeApp extends StatefulWidget {
 }
 
 class _HomeAppState extends State<HomeApp> {
+  // Create run app and test
+
+  // Add function
+
+  // Create logic
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  Timer? timer;
+  bool started = false;
+  List laps = [];
+
+  // Creating the stop timer function
+
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      started = false;
+    });
+  }
+
+  // Creating the reset function
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+
+      digitSeconds = "00";
+      digitMinutes = "00";
+      digitHours = "00";
+
+      started = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  // Creating the start timer function
+
+  void start() {
+    started = true;
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+
+      if (localSeconds > 59) {
+        if (localMinutes > 59) {
+          localHours++;
+          localMinutes = 0;
+        } else {
+          localMinutes++;
+          localSeconds = 0;
+        }
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+        digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitHours = (hours >= 10) ? "$hours" : "0$hours";
+        digitMinutes = (minutes >= 10) ? "$minutes" : "0$minutes";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +124,7 @@ class _HomeAppState extends State<HomeApp> {
               height: 20,
             ),
             Center(
-              child: Text("00:00:00",
+              child: Text("$digitHours:$digitMinutes:$digitSeconds",
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 70.0,
@@ -57,12 +132,40 @@ class _HomeAppState extends State<HomeApp> {
                   )),
             ),
             Container(
-              height: 400,
-              decoration: BoxDecoration(
-                color: Color(0XFF323F68),
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Color(0XFF323F68),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                // Let's add a list builder
+
+                child: ListView.builder(
+                  itemCount: laps.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Lap n${index + 1}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Text(
+                            "${laps[index]}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )),
             SizedBox(
               height: 20.0,
             ),
@@ -71,12 +174,14 @@ class _HomeAppState extends State<HomeApp> {
               children: [
                 Expanded(
                   child: RawMaterialButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      (!started) ? start() : stop();
+                    },
                     shape: StadiumBorder(
                       side: BorderSide(color: Colors.blue),
                     ),
                     child: Text(
-                      "Start",
+                      (!started) ? "Start" : "Pause",
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
@@ -86,12 +191,16 @@ class _HomeAppState extends State<HomeApp> {
                 ),
                 IconButton(
                   color: Colors.white,
-                  onPressed: () {},
+                  onPressed: () {
+                    addLaps();
+                  },
                   icon: Icon(Icons.flag),
                 ),
                 Expanded(
                   child: RawMaterialButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      reset();
+                    },
                     fillColor: Colors.blue,
                     shape: StadiumBorder(),
                     child: Text(
